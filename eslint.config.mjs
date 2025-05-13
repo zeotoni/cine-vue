0202b4b5f1b4efa6d0b088ca91f3488d8e8aea5b
zeotoni <zeotoni666@gmail.com>
2025-05-13 16:35:19 -0300

    🔧 chore: adds initial configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,74 @@
+import pluginJs from '@eslint/js';
+import vueConfigPrettier from '@vue/eslint-config-prettier';
+import vueConfigTypescript from '@vue/eslint-config-typescript';
+import prettier from 'eslint-plugin-prettier/recommended';
+import pluginVue from 'eslint-plugin-vue';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+  },
+  // js
+  pluginJs.configs.recommended,
+  {
+    rules: {
+      'no-unused-vars': 'off',
+      'no-undef': 'off',
+    },
+  },
+  // ts
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+    },
+  },
+  // vue
+  ...pluginVue.configs['flat/recommended'],
+  {
+    files: ['*.vue', '**/*.vue'],
+    languageOptions: {
+      parserOptions: {
+        parser: tseslint.parser,
+      },
+    },
+  },
+  {
+    rules: {
+      ...vueConfigTypescript.rules,
+      ...vueConfigPrettier.rules,
+      'prettier/prettier': [
+        'warn',
+        {
+          singleQuote: true,
+        },
+      ],
+      'vue/multi-word-component-names': 'off',
+      'vue/attribute-hyphenation': 'off',
+      'vue/no-v-html': 'off',
+      'vue/v-on-event-hyphenation': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+  {
+    ignores: ['node_modules', '.nuxt', '.output', 'dist'],
+  },
+  // prettier
+  prettier,
+  {
+    rules: {
+      'prettier/prettier': ['warn', { singleQuote: true }],
+    },
+  },
+];
