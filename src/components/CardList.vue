3081b231a326158f6d3f57497a9d4c4e085fd2b6
zeotoni <zeotoni666@gmail.com>
2025-05-29 10:49:12 -0300

    ✨ feat: adds CardList component with title prop and layout

---
+++ src/components/CardList.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<template>
+  <section class="container mx-auto px-4 py-8">
+    <h4 class="text-fs-3 font-fw3 text-primaryHeading mb-3">{{ title }}</h4>
+
+    <div
+      class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 overflow-x-auto gap-4"
+    ></div>
+  </section>
+</template>
