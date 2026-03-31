41bdc1d831ed4ae39ce1bc8a4dd3e15a06dbc093
zeotoni <zeotoni666@gmail.com>
2026-03-30 21:25:42 -0300

    ✨ feat: replaces static data with props

--- src/components/MovieDetailsModal.vue
+++ src/components/MovieDetailsModal.vue
@@ -1,7 +1,27 @@
 <script lang="ts">
+import fallbackImg from '@/assets/images/no-poster.png'
+import { genreMap } from '@/constants/genres'
+import type { MovieCard } from '@/interfaces/MovieCardData'
 import { Star } from 'lucide-vue-next'
+import type { PropType } from 'vue'
 export default {
   components: { Star },
+  props: {
+    movie: {
+      type: Object as PropType<MovieCard>,
+      required: true,
+    },
+  },
+  methods: {
+    getImgUrl(path: string) {
+      return path == null
+        ? fallbackImg
+        : `https://image.tmdb.org/t/p/original${path}`
+    },
+    getGenreName(id: number) {
+      return genreMap[id] || 'Unknown'
+    },
+  },
 }
 </script>
 
@@ -10,8 +30,8 @@ export default {
     <picture>
       <img
         class="object-cover w-full h-[400px] sm:h-[500px] md:h-[600px] rounded-xl"
-        src="../assets/images/no-poster.png"
-        alt="Imagem do filme"
+        :src="getImgUrl(movie?.backdrop_path)"
+        :alt="`Imagem do filme ` + movie?.title"
       />
     </picture>
 
@@ -22,9 +42,13 @@ export default {
     <div
       class="absolute inset-0 flex flex-col justify-end p-4 sm:p-6 md:p-8 w-full"
     >
-      <h2 class="text-fs-4 font-fw3 text-primaryHeading mb-2">The GodFather</h2>
+      <h2 class="text-fs-4 font-fw3 text-primaryHeading mb-2">
+        {{ movie?.title }}
+      </h2>
 
-      <time datetime="1999" class="text-primaryText text-fs-1 mb-2">1999</time>
+      <time datetime="1999" class="text-primaryText text-fs-1 mb-2">{{
+        movie?.release_date
+      }}</time>
 
       <div class="flex flex-wrap gap-3 items-center mb-4">
         <div class="flex gap-1 items-center">
@@ -34,21 +58,22 @@ export default {
             color="#FBBF24"
             aria-hidden="true"
           />
-          <span class="text-fs-1 font-fw2 text-rating">8.7</span>
+          <span class="text-fs-1 font-fw2 text-rating">{{
+            movie?.vote_average
+          }}</span>
         </div>
 
         <span
+          v-for="id in movie?.genre_ids"
+          :key="id"
           class="text-fs-1 font-fw2 text-primaryHeading px-2 py-0.5 bg-white/30 rounded-full"
-          >Drama</span
+          >{{ getGenreName(id) }}</span
         >
       </div>
       <p
         class="text-primaryText text-fs-2 max-w-3xl leading-relaxed line-clamp-4 overflow-y-auto scrollbar-hide md:line-clamp-none [-ms-overflow-style:'none'] [scrollbar-width:'none'] [-webkit-overflow-scrolling:touch] [&::-webkit-scrollbar]:hidden"
       >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure doloremque
-        explicabo omnis, at neque animi hic praesentium facilis adipisci aliquid
-        suscipit maiores molestiae dolore obcaecati dolores necessitatibus
-        maxime. Facilis, hic.
+        {{ movie?.overview }}
       </p>
     </div>
   </dialog>
