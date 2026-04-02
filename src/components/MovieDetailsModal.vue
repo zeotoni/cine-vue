1cc62df23f6bdb56da6e7fe87653d0b3260806cc
zeotoni <zeotoni666@gmail.com>
2026-04-02 00:50:11 -0300

    ✨ feat: adds modal transition animation

--- src/components/MovieDetailsModal.vue
+++ src/components/MovieDetailsModal.vue
@@ -110,66 +110,77 @@ export default {
     ref="movieModal"
     class="relative m-auto w-[80%] overflow-hidden rounded-xl"
   >
-    <button
-      class="absolute m-2 lg:m-4 right-0 bg-black/60 backdrop-blur shadow-md border border-white/30 rounded"
-      @click="closeModal"
+    <Transition
+      enter-active-class="transition duration-1000 ease-out"
+      enter-from-class="opacity-0"
+      enter-to-class="opacity-100"
+      leave-active-class="transition duration-1000 ease-in"
+      leave-from-class="opacity-100"
+      leave-to-class="opacity-0"
     >
-      <X
-        class="w-5 h-5 md:w-8 md:h-8"
-        fill="#FFF"
-        color="#FFF"
-        aria-hidden="true"
-      />
-    </button>
-    <picture>
-      <img
-        :key="movie?.backdrop_path"
-        class="object-cover w-full h-[400px] sm:h-[500px] md:h-[600px] rounded-xl"
-        :src="getImgUrl(movie?.backdrop_path)"
-        :alt="`Imagem do filme ` + movie?.title"
-      />
-    </picture>
-
-    <div
-      class="absolute inset-0 bg-gradient-to-t from-black to-transparent pointer-events-none"
-    ></div>
-
-    <div
-      class="absolute inset-0 flex flex-col justify-end p-4 sm:p-6 md:p-8 w-full pointer-events-none"
-    >
-      <h2 class="text-fs-4 font-fw3 text-primaryHeading mb-2">
-        {{ movie?.title }}
-      </h2>
-
-      <time datetime="1999" class="text-primaryText text-fs-1 mb-2">{{
-        movie?.release_date
-      }}</time>
-
-      <div class="flex flex-wrap gap-3 items-center mb-4">
-        <div class="flex gap-1 items-center">
-          <Star
-            class="w-5 h-5"
-            fill="#FBBF24"
-            color="#FBBF24"
+      <div v-if="shouldOpen">
+        <button
+          class="absolute m-2 lg:m-4 right-0 bg-black/60 backdrop-blur shadow-md border border-white/30 rounded"
+          @click="closeModal"
+        >
+          <X
+            class="w-5 h-5 md:w-8 md:h-8"
+            fill="#FFF"
+            color="#FFF"
             aria-hidden="true"
           />
-          <span class="text-fs-1 font-fw2 text-rating">{{
-            movie?.vote_average
-          }}</span>
-        </div>
+        </button>
+        <picture>
+          <img
+            :key="movie?.backdrop_path"
+            class="object-cover w-full h-[400px] sm:h-[500px] md:h-[600px] rounded-xl"
+            :src="getImgUrl(movie?.backdrop_path)"
+            :alt="`Imagem do filme ` + movie?.title"
+          />
+        </picture>
 
-        <span
-          v-for="id in movie?.genre_ids"
-          :key="id"
-          class="text-fs-1 font-fw2 text-primaryHeading px-2 py-0.5 bg-white/30 rounded-full"
-          >{{ getGenreName(id) }}</span
+        <div
+          class="absolute inset-0 bg-gradient-to-t from-black to-transparent pointer-events-none"
+        ></div>
+
+        <div
+          class="absolute inset-0 flex flex-col justify-end p-4 sm:p-6 md:p-8 w-full pointer-events-none"
         >
+          <h2 class="text-fs-4 font-fw3 text-primaryHeading mb-2">
+            {{ movie?.title }}
+          </h2>
+
+          <time datetime="1999" class="text-primaryText text-fs-1 mb-2">{{
+            movie?.release_date
+          }}</time>
+
+          <div class="flex flex-wrap gap-3 items-center mb-4">
+            <div class="flex gap-1 items-center">
+              <Star
+                class="w-5 h-5"
+                fill="#FBBF24"
+                color="#FBBF24"
+                aria-hidden="true"
+              />
+              <span class="text-fs-1 font-fw2 text-rating">{{
+                movie?.vote_average
+              }}</span>
+            </div>
+
+            <span
+              v-for="id in movie?.genre_ids"
+              :key="id"
+              class="text-fs-1 font-fw2 text-primaryHeading px-2 py-0.5 bg-white/30 rounded-full"
+              >{{ getGenreName(id) }}</span
+            >
+          </div>
+          <p
+            class="text-primaryText text-fs-2 max-w-3xl leading-relaxed line-clamp-4 overflow-y-auto scrollbar-hide md:line-clamp-none [-ms-overflow-style:'none'] [scrollbar-width:'none'] [-webkit-overflow-scrolling:touch] [&::-webkit-scrollbar]:hidden"
+          >
+            {{ movie?.overview }}
+          </p>
+        </div>
       </div>
-      <p
-        class="text-primaryText text-fs-2 max-w-3xl leading-relaxed line-clamp-4 overflow-y-auto scrollbar-hide md:line-clamp-none [-ms-overflow-style:'none'] [scrollbar-width:'none'] [-webkit-overflow-scrolling:touch] [&::-webkit-scrollbar]:hidden"
-      >
-        {{ movie?.overview }}
-      </p>
-    </div>
+    </Transition>
   </dialog>
 </template>
