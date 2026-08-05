7fb2d3fd346924c59cf0b42790c35899daffc579
zeotoni <zeotoni666@gmail.com>
2026-08-05 20:30:35 -0300

    ✨ feat: creates footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,13 @@
+<script lang="ts"></script>
+
+<template>
+  <footer
+    class="border-t border-white/20 text-white p-4 md:p-6 lg:p-8 flex flex-col gap-2 bg-black/30"
+  >
+    <p class="text-xl font-bold">CineVue</p>
+    <p class="text-fs-1 font-fw1">
+      Discover movies from different regions around the world.
+    </p>
+    <p class="text-fs-1 font-fw1 mt-4">© 2026 CineVue</p>
+  </footer>
+</template>
